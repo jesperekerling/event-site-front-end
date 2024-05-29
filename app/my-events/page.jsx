ade9e131b39c3fea14e846d28999b87913833c37
Jesper Ekerling <jesper@forme.se>
2024-05-29 17:54:08 +0200

    Updated img to Image component

--- app/my-events/page.jsx
+++ app/my-events/page.jsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from 'react'
 import { useUser } from '@clerk/clerk-react'
+import Image from 'next/image'
 
 function ShowMyEvents() {
   const [events, setEvents] = useState([])
@@ -30,9 +31,14 @@ function ShowMyEvents() {
         {Array.isArray(events) && events.map((event, index) => (
           <div key={index}>
             <h2>{event.title}</h2>
-            <img src={event.image} alt={event.title} />
+            <Image
+              src={event.image}
+              alt={event.description}
+              width={500}
+              height={300}
+              className='object-cover aspect-video hover:opacity-75 transition-opacity duration-200'
+            />
             <p>{event.date} - {event.location}</p>
-            <p>{event.description}</p>
             <p>Price: {event.price}</p>
             <p>Seats: {event.seats}</p>
           </div>
